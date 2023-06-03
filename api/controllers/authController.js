debf9bc936f5501f67865b6389c4f08d2debf2a3
topgirl2410 <maria.topgirl82@gmail.com>
2023-06-03 15:01:21 +0200

    acabado registro e inicio de sesion

--- api/controllers/authController.js
+++ api/controllers/authController.js
@@ -1,5 +1,7 @@
 import { db } from '../db.js';
-import bcrypt from 'bcryptjs'
+import bcrypt from 'bcryptjs';
+import jwt from 'jsonwebtoken';
+
 
 export const register = (req, res) => {
     // Check existing user
@@ -34,6 +36,29 @@ export const register = (req, res) => {
 }
 
 export const login = (req, res) => {
+    // Check user 
+
+    const q = "SELECT * FROM users WHERE username = ?";
+    db.query(q, [
+        req.body.username
+    ], (err, data) => {
+        if (err) return res.json(err);
+        if (data.length === 0) return res.status(404).json("User not found!");
+
+        // Check password 
+        const isPasswordCorrect = bcrypt.compareSync(req.body.password, data[0].password);
+
+        if (!isPasswordCorrect) return res.status(400).json("Wrong username or password!");
+
+        const token = jwt.sign({ id: data[0].id }, "jwtkey");
+
+        const { password, ...other } = data[0]
+
+        res.cookie("access_token", token, {
+            httpOnly: true
+        }).status(200).json(other)
+
+    });
 
 }
 
--- api/server.js
+++ api/server.js
@@ -4,12 +4,16 @@ import authRoutes from './routes/auth.js'
 import usersRoutes from './routes/users.js'
 
 import cors from 'cors'
+import cookieParser from 'cookie-parser';
 
 const app = express();
 
 app.use(express.json());
 
-app.use(cors())
+app.use(cookieParser());
+
+app.use(cors());
+
 
 
 app.use("/api/posts/", postRoutes);
