ba3ddf674f68bd51c5c84fe11fcc3d8dc14d717b
topgirl2410 <maria.topgirl82@gmail.com>
2023-06-04 00:09:31 +0200

    mi primer post publicado

--- api/controllers/postController.js
+++ api/controllers/postController.js
@@ -1,3 +1,28 @@
+import { db } from "../db.js"
+
+export const getPosts = (req, res) => {
+    const q = req.query.cat ? "SELECT * FROM posts WHERE cat=?"
+        : "SELECT * FROM posts";
+
+    db.query(q, [req.query.cat], (err, data) => {
+        if (err) return res.send(err);
+
+        return res.status(200).json(data)
+    })
+}
+
+export const getPost = (req, res) => {
+
+}
+
 export const addPost = (req, res) => {
-    res.json("from controller")
+    res.json("addpost")
+}
+
+export const deletePost = (req, res) => {
+    res.json("deletepost")
+}
+
+export const updatePost = (req, res) => {
+    res.json("update")
 }
